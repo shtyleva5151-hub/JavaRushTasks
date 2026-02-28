f442e759d223083c1a3cbe905dbd76731a31f730
shtyleva5151-hub <shtyleva5151@gmail.com>
2026-02-28 19:33:10 +0300

    Add cod\task04\03

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task04.task0403;
+
+import java.util.Scanner;
+
+/* 
+Суммирование
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum = sum + number;
+            } else if (scanner.hasNextLine()) {
+                String str = scanner.nextLine();
+                if (str.equals("ENTER")) {
+                    break;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
